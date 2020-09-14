a8f3b640e48b5b23109cf6470efae03277bfcb86
florbrunet <florbrunet@@hotmail.com>
2020-09-14 19:02:01 -0300

    Actualization2

---
+++ Ejercicio1.js
@@ -0,0 +1,6 @@
+let fecha= new Date()
+console.log('Hoy es el día '+ fecha.getDate() +' del mes '+ (fecha.getMonth() +1) +' del año '+ fecha.getFullYear());
+console.log('Buen dia');
+
+//let fecha= new Date()
+//console.log('Hoy es el día '+ fecha.getDate() +' del mes '+ (fecha.getMonth() +1) +' del año '+ fecha.getFullYear());
